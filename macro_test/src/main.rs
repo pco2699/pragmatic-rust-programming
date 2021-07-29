c373b55c4aa3cf4c9be89203a9aae3da96ac0690
pco2699 <aenkun@gmail.com>
2021-07-29 22:38:53 +0900

    feature: macro samples

--- macro_test/src/main.rs
+++ macro_test/src/main.rs
@@ -1,3 +1,33 @@
+macro_rules! five_times {
+    ($x:expr) => {
+        5 * $x
+    };
+}
+
+macro_rules! vec {
+    ( $x:ty ) => {
+        {
+            let temp_vec: Vec<$x> = Vec::new();
+            temp_vec
+        }
+    };
+    ( $( $x:expr ),*) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(25, five_times!(2 + 3));
+    let x = vec!(0,1,2);
+    println!("{:?}", x);
+    let y = vec!(i32);
+    println!("{:?}", y);
+
 }
---
+++ sample_macro/src/main.rs
@@ -0,0 +1,15 @@
+use tomlstruct::tomlstruct;
+
+
+tomlstruct! {
+    [Hello]
+    name = "hello"
+    version = 1.0
+}
+
+fn main() {
+    let _ = Hello {
+        name: String::from("hello"),
+        version: 1.0,
+    };
+}
